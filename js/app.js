094a98f36bceb9c7db66a6b2095d6d13a2e55317
Anik <aniksarker1612@gmail.com>
2020-09-20 18:45:29 +0800

    file src issue fix

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const sem_1 = document.getElementById('sem-1');
+const sem_2 = document.getElementById('sem-2');
+const sem_3 = document.getElementById('sem-3');
+const sem_4 = document.getElementById('sem-4');
+const sem_5 = document.getElementById('sem-5');
+const sem_6 = document.getElementById('sem-6');
+const sem_7 = document.getElementById('sem-7');
+const sem_8 = document.getElementById('sem-8');
+const result = document.getElementById('result');
+
+const handleSubmit = (e) => {
+    result.value = 'anik'
+}
